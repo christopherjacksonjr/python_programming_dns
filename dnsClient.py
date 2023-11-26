c58de144aa6b1a53c2b721c66a423e5c6eea33aa
Christopher Jackson <jacksonchristopher0924@gmail.com>
2023-11-26 13:08:20 -0600

    changing port number

--- dnsClient.py
+++ dnsClient.py
@@ -10,7 +10,7 @@ import struct
 def dns_query(type, name, server):
     # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = (server, 13331) # Enter Port Number
+    server_address = (server, 53) # Enter Port Number
 
     # Create the DNS query
     ID = 0x1234
@@ -82,7 +82,7 @@ def dns_query(type, name, server):
         
     qname_parts = name.split('.') # How can we easily split the string?
     qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('utf-8') for part in qname_parts] # Make sure it's encoded as a sequence of the right character encoding type (lowercase)
-    qname_encoded = b''.join(qname_encoded_parts) + b'\x80' #enter the closing byte value to signify the end of the domain string (two digits)
+    qname_encoded = b''.join(qname_encoded_parts) + b'\x00' #enter the closing byte value to signify the end of the domain string (two digits)
 
     # Encode the QTYPE and QCLASS
 
